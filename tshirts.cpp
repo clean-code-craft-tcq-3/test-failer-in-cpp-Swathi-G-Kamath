a232cca72310c0e2800d9b1de312cfd47f212ed7
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-04-26 20:15:07 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -13,12 +13,17 @@ char size(int cms) {
     return sizeName;
 }
 
-int main() {
+void checkTshirtSizes()
+{
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     assert(size(38) == 'M');
     assert(size(42) == 'M');
+}
+
+int main() {
+    checkTshirtSizes();   
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
