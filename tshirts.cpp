23db05f295b0aca0f654bad002dbf2ab1819edf4
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-05-02 19:13:20 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <assert.h>
-#include "tshirts.hpp"
-#include "tshirts_test.hpp"
 
 char size(int cms) {
     char sizeName = '\0';
@@ -15,6 +13,15 @@ char size(int cms) {
         sizeName = 'L';
     }
     return sizeName;
+}
+
+void checkTshirtsSize()
+{
+    assert(size(37) == 'S');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+    assert(size(38) == 'M');
+    assert(size(42) == 'M');
 }
 
 int main()
